1ca7eabd36542dd72916819c263e740b1b31cdff
Ama003 <amanuelademe2@gmail.com>
2022-11-03 12:52:56 +0300

    login and register and welcome page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,8 @@
 import 'package:cinema/utils/constant.dart';
 import 'package:flutter/material.dart';
 
+import '../welocme.dart';
+
 class Login extends StatefulWidget {
   const Login({super.key});
 
@@ -13,97 +15,107 @@ class _LoginState extends State<Login> {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(title: Text("A Cinema"), backgroundColor: Colors.black),
       body: Body(),
-     );
+    );
   }
 
   Padding Body() {
     return Padding(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          Container(
-              alignment: Alignment.topLeft,
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Log in',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+            Container(
               padding: const EdgeInsets.all(10),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                controller: nameController,
+                decoration: const InputDecoration(
+                    hintText: "Type your email",
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                    hintText: "Type your password",
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red)),
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    suffixIcon: Icon(
+                      Icons.visibility,
+                    )),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
               child: const Text(
-                'Log in',
-                style: TextStyle(fontSize: 20, color: Colors.white), 
-              )),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              controller: nameController,
-              decoration: const InputDecoration(
-                hintText: "Type your email",
-                hintStyle: TextStyle(color: Colors.white),
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.white,)
+                'Forgot Password',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-            child: TextField(
-              style: TextStyle(color: Colors.white), 
-              obscureText: true,
-              controller: passwordController,
-              decoration: const InputDecoration(
-                hintText: "Type your password",
-                hintStyle: TextStyle(color: Colors.white),
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white),
-                suffixIcon: Icon(
-                  Icons.visibility,
-                )
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              //forgot password screen
-            },
-            child: const Text('Forgot Password', style: TextStyle(color: Colors.white),),
-          ),
-          Container(
+            Container(
               height: 50,
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Constants.primaryColor),
-                child: const Text('Login', style: TextStyle(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Constants.primaryColor),
+                child:
+                    const Text('Login', style: TextStyle(color: Colors.white)),
                 onPressed: () {
-                  print(nameController.text);
-                  print(passwordController.text);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Welcome()),
+                  );
                 },
-              )
-          ),
-          Row(
-            children: <Widget>[
-              const Text('Does not have account?', style: TextStyle(color: Colors.white),),
-              TextButton(
-                child: const Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Does not have account?',
+                  style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {
-
-                },
-              )
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ],
-    
-      )
-    );
+                TextButton(
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
---
+++ lib/welocme.dart
@@ -0,0 +1,143 @@
+import 'package:cinema/utils/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/components/button/gf_button.dart';
+import 'package:getwidget/shape/gf_button_shape.dart';
+import 'package:getwidget/types/gf_button_type.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(
+                Icons.manage_accounts,
+                size: 35,
+                color: Colors.red,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: const Icon(
+                Icons.logout,
+                size: 35,
+                color: Colors.red,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '  Welcome',
+                style: TextStyle(fontSize: 60, color: Colors.red),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '  Full Name',
+                style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '  Email',
+                style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '  Address',
+                style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '  Contact',
+                style: TextStyle(
+                  fontSize: 27,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 200,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: GFButton(
+                    color: Constants.primaryColor,
+                    text: "View Booking",
+                    size: 42,
+                    shape: GFButtonShape.pills,
+                    blockButton: true,
+                    type: GFButtonType.outline2x,
+                    fullWidthButton: true,
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => const Register()),
+                      // );
+                    },
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Expanded(
+                  child: GFButton(
+                    color: Constants.primaryColor,
+                    onPressed: () {},
+                    text: "Browse Movies",
+                    size: 42,
+                    shape: GFButtonShape.pills,
+                    blockButton: true,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
